dfc1a623eadbc753554c3a03f149cfcd4caa0901
Melvyn de Kort <melvyn@mdekort.nl>
2025-10-03 20:14:16 +0200

    Rename dockersetup repo to lmserver

--- terraform/repositories.tf
+++ terraform/repositories.tf
@@ -149,7 +149,7 @@ resource "github_actions_secret" "repo_secrets" {
     "secrets.codecov.scheduler-token"            = local.secrets.codecov.scheduler-token
     "secrets.codecov.image-refresher-token"      = local.secrets.codecov.image-refresher-token
     "secrets.codecov.get-cookies-token"          = local.secrets.codecov.get-cookies-token
-    "github_actions_dockersetup_role_arn"        = aws_iam_role.github_actions["dockersetup"].arn
+    "github_actions_lmserver_role_arn"           = aws_iam_role.github_actions["lmserver"].arn
     "github_actions_ignition_role_arn"           = aws_iam_role.github_actions["ignition"].arn
     "github_actions_tf_github_role_arn"          = data.aws_iam_role.tf_github_role.arn
     "github_actions_tf_cloudflare_role_arn"      = aws_iam_role.github_actions["tf-cloudflare"].arn
