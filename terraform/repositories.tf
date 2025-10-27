084b7cd77920f7106d4326d5062d0b0a23ea94e1
Melvyn de Kort <melvyn@mdekort.nl>
2025-10-27 17:40:14 +0100

    Remove lmgateway, since we use Cloudflare Zero Trust as a tunneling mechanism

--- terraform/repositories.tf
+++ terraform/repositories.tf
@@ -163,7 +163,6 @@ resource "github_actions_secret" "repo_secrets" {
     "github_actions_mta_sts_role_arn"            = aws_iam_role.github_actions["mta-sts"].arn
     "github_actions_get_cookies_role_arn"        = aws_iam_role.github_actions["get-cookies"].arn
     "github_actions_example_melvyn_dev_role_arn" = aws_iam_role.github_actions["example.melvyn.dev"].arn
-    "github_actions_lmgateway_role_arn"          = aws_iam_role.github_actions["lmgateway"].arn
     "github_actions_startpage_role_arn"          = aws_iam_role.github_actions["startpage"].arn
   }, each.value.value_ref, "")
 }
