466dc5cb69faadc7511bc9b37fbcefb12d521d6e
Melvyn de Kort <melvyn@mdekort.nl>
2026-03-15 11:42:44 +0100

    Migrate state to consolidated tfstate bucket

--- terraform/remote-state.tf
+++ terraform/remote-state.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "tf_cloudflare" {
   backend = "s3"
 
   config = {
-    bucket = "mdekort.tfstate"
+    bucket = "mdekort-tfstate-075673041815"
     key    = "tf-cloudflare.tfstate"
     region = "eu-west-1"
   }
